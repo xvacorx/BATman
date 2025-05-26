10f57f1ed50cb90cc3db3d1b3d6254ffcafc4704
xvacorx <theproofthevictors@gmail.com>
2025-05-26 11:15:21 -0300

    updated Dynamic path and file name

--- Generate Hash/GenerateHash.bat
+++ Generate Hash/GenerateHash.bat
@@ -1,1 +1,13 @@
-MDMDiagnosticsTool -area autopilot -cab C:\hash.cab
+@echo off
+setlocal
+
+for %%P in ("%~dp0.") do set "current_drive=%%~dP"
+
+for /f "tokens=2 delims==" %%s in ('wmic bios get serialnumber /value ^| find "SerialNumber="') do set "serial_number=%%s"
+
+set "cab_path=%current_drive%%serial_number%.cab"
+
+MDMDiagnosticsTool -area autopilot -cab "%cab_path%"
+
+endlocal
+pause
