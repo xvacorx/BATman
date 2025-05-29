bc3d7e6b89c5592462a54f4d2f68ab89155031c2
Victor Sanchez <130621028+xvacorx@users.noreply.github.com>
2025-05-29 12:28:41 -0300

    Update ClearLogs.bat

--- Shutdown Timeout/ClearLogs.bat
+++ Shutdown Timeout/ClearLogs.bat
@@ -3,5 +3,10 @@ DEL "%TEMP%\shutdown_log.txt" 2>nul
 DEL "%TEMP%\last_activity_timestamp.txt" 2>nul
 DEL "%TEMP%\warning_sent_flag.txt" 2>nul
 DEL "%TEMP%\shutdown_monitor_test_status.txt" 2>nul
-echo All related temporary files have been removed.
+
+:: Attempt to delete ShutdownTimeout.bat from the Startup folder
+SET "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+DEL "%STARTUP_FOLDER%\ShutdownTimeout.bat" 2>nul
+
+echo All related temporary files and the startup script have been removed.
 timeout /t 3 /nobreak >nul
